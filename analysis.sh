900b7db566225d9f8394a91d8cac91b4ae10c10b
floryhyy <38958428+floryhyy@users.noreply.github.com>
2021-09-20 21:59:32 -0500

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-spred_path='/c/Users/flori.LAPTOP-SAJS3KG7/Documents/GitHub/sanchez-roige-lab/MetaXcan/software'
-/home/MetaXcan/software
-hmagma_path='/c/Users/flori.LAPTOP-SAJS3KG7/Documents/GitHub/H-MAGMA/Codes'
-ldsc_path='/c/Users/flori.LAPTOP-SAJS3KG7/Documents/GitHub/ldsc'
+spred_path='/home/MetaXcan/software'
+hmagma_path='/home/H-MAGMA/Codes'
+ldsc_path='/home/GitHub/ldsc'
 current=$PWD
 
 filename='chr1.assoc.dosage.gz'
@@ -25,12 +24,12 @@ echo $spredxcan
 eval $spredxcan
 
 #run hmagma
-cd $current
-cd $current
-hmagma=`python run_analysis.py $current $filename $snp $beta $z $p $a1 $a2 $ncol $n $ignore  hmagma`
-cd $hmagma_path
-echo $hmagma
-eval $hmagma
+# cd $current
+# cd $current
+# hmagma=`python run_analysis.py $current $filename $snp $beta $z $p $a1 $a2 $ncol $n $ignore  hmagma`
+# cd $hmagma_path
+# echo $hmagma
+# eval $hmagma
 
 # run ldsc
 # cd $current
