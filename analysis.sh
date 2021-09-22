6c28e5ac80d2f8487bea35a5209794ddff6279c0
floryhyy <38958428+floryhyy@users.noreply.github.com>
2021-09-22 14:53:20 -0500

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -18,26 +18,21 @@ filename=${10}
 
 
 #run Metaxcan
-# spredxcan=`python run_analysis.py $current $filename spred $snp $beta $z $p $a1 $a2 $ncol $n $ignore`
-# cd $spred_path
-# echo $spredxcan
-# eval $spredxcan
+spredxcan=`python run_analysis.py $current $filename spred $snp $beta $z $p $a1 $a2 $ncol $n $ignore`
+cd $spred_path
+echo $spredxcan
+eval $spredxcan
 
 #run hmagma
-# cd $current
-# hmagma=`python run_analysis.py $current $filename hmagma $snp $beta $z $p $a1 $a2 $ncol $n $ignore`
-# cd $hmagma_path
-# echo $hmagma
-# eval $hmagma
+cd $current
+hmagma=`python run_analysis.py $current $filename hmagma $snp $beta $z $p $a1 $a2 $ncol $n $ignore`
+cd $hmagma_path
+echo $hmagma
+eval $hmagma
 
 #run ldsc
 cd $current
 ldsc=`python run_analysis.py $current $filename ldsc $snp $beta $z $p $a1 $a2 $ncol $n $ignore`
-
-# #if you are currently in enviorment with python3, switch enviorment below
-# eval "$(conda shell.bash hook)"
-# conda activate python27
-
 cd $ldsc_path
 echo $ldsc
 eval $ldsc
